7662e6c738c542c8e72b75de9fdd9199b52ed790
Katie Rose Felten <katiefelten@gmail.com>
2020-07-22 21:03:47 -0400

    :tada: lettuce begin, shall we

---
+++ singly_linked_list/singly_linked_list.py
@@ -0,0 +1,36 @@
+class Node:
+    def __init__(self, value=None, next_node=None):
+        # the value at this linked list node
+        self.value = value
+        # reference to the next node in the list
+        self.next_node = next_node
+    def get_value(self):
+        return self.value
+    def get_next(self):
+        return self.next_node
+    def set_next(self, new_next):
+        # set this node's next_node reference to the passed in node
+        self.next_node = new_next
+
+class LinkedList: 
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.length = 0
+
+    def add_to_head(self, value):
+        new_node = Node(value, self.head)
+        self.head = new_node
+        if self.length == 0: 
+            self.tail = new_node
+        self.length += 1
+
+    def add_to_tail(self, value):
+        new_node = Node(value)
+        if self.head is None and self.tail is None:
+            self.head = new_node
+        else: 
+            self.tail.set_next(new_node)
+            self.tail = new_node
+            self.length += 1
+
