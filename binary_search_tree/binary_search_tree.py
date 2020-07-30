b7d07a1756ffdf819ea9c7492b3188f32395ae99
Katie Rose Felten <katiefelten@gmail.com>
2020-07-29 23:32:04 -0400

    :zap: binary search trees

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -1,3 +1,4 @@
+
 """
 Binary search trees are a data structure that enforce an ordering over 
 the data they store. That ordering in turn makes it a lot more efficient 
@@ -9,6 +10,152 @@ This part of the project comprises two days:
 2. Implement the `in_order_print`, `bft_print`, and `dft_print` methods
    on the BSTNode class.
 """
+
+class DoublyLinkedList:
+    def __init__(self, node=None):
+        self.head = node
+        self.tail = node
+        self.length = 1 if node is not None else 0
+
+    def __len__(self):
+        return self.length
+
+    """Wraps the given value in a ListNode and inserts it 
+    as the new head of the list. Don't forget to handle 
+    the old head node's previous pointer accordingly."""
+
+    def add_to_head(self, value):
+        new_node = ListNode(value, None, None)
+        self.length += 1
+        # if list is empty
+        if not self.head and not self.tail:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head.prev = new_node
+            self.head = new_node
+
+    """Removes the List's current head node, making the
+    current head's next node the new head of the List.
+    Returns the value of the removed Node."""
+
+    def remove_from_head(self):
+        value = self.head.value
+        self.delete(self.head)
+        return value
+
+    """Wraps the given value in a ListNode and inserts it 
+    as the new tail of the list. Don't forget to handle 
+    the old tail node's next pointer accordingly."""
+
+    def add_to_tail(self, value):
+        new_node = ListNode(value, None, None)
+        self.length += 1
+        # if list is empty
+        if not self.head and not self.tail:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.prev = self.tail
+            self.tail.next = new_node
+            self.tail = new_node
+
+    """Removes the List's current tail node, making the 
+    current tail's previous node the new tail of the List.
+    Returns the value of the removed Node."""
+
+    def remove_from_tail(self):
+        value = self.tail.value
+        self.delete(self.tail)
+        return value
+
+    """Removes the input node from its current spot in the 
+    List and inserts it as the new head node of the List."""
+
+    def move_to_front(self, node):
+        value = node.value
+        self.delete(node)
+        self.add_to_head(value)
+
+    """Removes the input node from its current spot in the 
+    List and inserts it as the new tail node of the List."""
+
+    def move_to_end(self, node):
+        value = node.value
+        self.delete(node)
+        self.add_to_tail(value)
+
+    """Removes a node from the list and handles cases where
+    the node was the head or the tail"""
+
+    def delete(self, node):
+        self.length -= 1
+        # only 1 item in the list
+        if self.head == self.tail:
+            self.head = None
+            self.tail = None
+        elif self.head == node:
+            self.head = self.head.next
+            node.delete()
+        elif self.tail == node:
+            self.tail = self.tail.prev
+            node.delete()
+        else:
+            node.delete()
+
+    """Returns the highest value currently in the list"""
+
+    def get_max(self):
+        if not self.head:
+            return None
+        max_value = self.head.value
+        current = self.head
+        while current:
+            if current.value > max_value:
+                max_value = current.value
+            current = current.next
+        return max_value
+
+class Queue:
+    def __init__(self):
+        self.size = 0
+        # self.storage = ?
+        self.storage = DoublyLinkedList()
+    
+    def __len__(self):
+        return self.size
+
+    def enqueue(self, value):
+        self.storage.add_to_tail(value)
+        self.size += 1
+
+    def dequeue(self):
+        if self.size > 0:
+            self.size -= 1
+            return self.storage.remove_from_head()
+
+
+class Stack:
+    def __init__(self):
+        self.size = 0
+        # self.storage = ?
+        self.storage = DoublyLinkedList()
+
+    def __len__(self):
+        return self.size
+
+    def push(self, value):
+        self.storage.add_to_tail(value)
+        self.size += 1
+
+    def pop(self):
+        if not self.storage.head:
+            return
+        else:
+            self.size -= 1
+            return self.storage.remove_from_tail()
+
 class BSTNode:
     def __init__(self, value):
         self.value = value
@@ -17,37 +164,97 @@ class BSTNode:
 
     # Insert the given value into the tree
     def insert(self, value):
-        pass
 
-    # Return True if the tree contains the value
-    # False if it does not
+            #Root occupied when class is instantiated, check whether it is smaller or bigger, respectively goes to left or right
+        if(value < self.value):
+            if self.left == None: #if there is no leaf, spawn new tree node
+                self.left = BSTNode(value)
+            else:
+                self.left.insert(value)#otherwise attempt insert from left node as starting node 
+        elif(value >= self.value):
+            if self.right == None:
+                self.right = BSTNode(value)
+            else:
+                self.right.insert(value)
+
+
+ 
     def contains(self, target):
-        pass
+        if target < self.value: #If less, check if node is empty, then reached base case and target doesn't exist in the tree
+            if self.left is None:
+                return False
+            return self.left.contains(target) #Otherwise recursion from left node and try finding it
+        elif target > self.value:
+            if self.right is None:
+                return False
+            return self.right.contains(target)
+        else: #if target value isn't less or greater, then it found the node
+            return True
 
     # Return the maximum value found in the tree
     def get_max(self):
-        pass
+        if self.right == None:
+            return self.value
+        else:
+            return self.right.get_max()
 
     # Call the function `fn` on the value of each node
     def for_each(self, fn):
-        pass
+        if self.right:
+            self.right.for_each(fn)
+        if self.left:
+            self.left.for_each(fn)
+        fn(self.value)
 
     # Part 2 -----------------------
 
     # Print all the values in order from low to high
     # Hint:  Use a recursive, depth first traversal
-    def in_order_print(self):
-        pass
+    def in_order_print(self, node):
+        if node:
+            #start left, print to the root, then print the right ones
+            self.in_order_print(node.left)
+            print(node.value)
+            self.in_order_print(node.right)
 
     # Print the value of every node, starting with the given node,
     # in an iterative breadth first traversal
-    def bft_print(self):
-        pass
+    def bft_print(self, node):
+        #Using Queue DS
+        queue = Queue()
+
+        #Enqueue the node and loop through
+        queue.enqueue(node)
+
+        while queue.size>0:
+            #Remove from queue and Make it current node to print and iterate through
+            current = queue.dequeue()
+            if current:
+                print(current.value)
+            if current.left:
+                queue.enqueue(current.left)
+            if current.right:
+                queue.enqueue(current.right)
 
     # Print the value of every node, starting with the given node,
     # in an iterative depth first traversal
-    def dft_print(self):
-        pass
+    def dft_print(self, node):
+        #Using Stack DS
+        stack = Stack()
+
+        #Push the node and Loop through
+        stack.push(node)
+
+        while stack.size> 0:
+            #Remove the stack to make it current node and print and iterate through
+            current = stack.pop()
+
+            if current:
+                print(current.value)
+            if current.left:
+                stack.push(current.left)
+            if current.right:
+                stack.push(current.right)
 
     # Stretch Goals -------------------------
     # Note: Research may be required
@@ -63,7 +270,7 @@ class BSTNode:
 """
 This code is necessary for testing the `print` methods
 """
-bst = BinarySearchTree(1)
+bst = BSTNode(1)
 
 bst.insert(8)
 bst.insert(5)
@@ -76,10 +283,10 @@ bst.insert(2)
 bst.bft_print()
 bst.dft_print()
 
-print("elegant methods")
-print("pre order")
-bst.pre_order_dft()
-print("in order")
-bst.in_order_dft()
-print("post order")
-bst.post_order_dft()  
+#print("elegant methods")
+#print("pre order")
+#bst.pre_order_dft()
+#print("in order")
+#bst.in_order_dft()
+#print("post order")
+# bst.post_order_dft()  
